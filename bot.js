61a09b8620bcb62cbc179d2e0a71c0f92c66098b
kinglalu <56965132+kinglalu@users.noreply.github.com>
2020-08-13 13:59:40 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,24 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
+ 
 const prefix = '-';
  
-
-
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
+const fs = require('fs');
+ 
+client.commands = new Discord.Collection();
+ 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+ 
+    client.commands.set(command.name, command);
+}
+ 
+ 
+client.once('ready', () => {
+    console.log('bot online');
 });
+ 
 
  
 
@@ -19,6 +28,16 @@ client.on('message', message =>{
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
+	client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === 'hey'){
+        client.commands.get('hey').execute(message, args);
+    } 
+});
     if(command === 'ping'){
         message.channel.send('pong!');
      
