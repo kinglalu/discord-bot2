7b5b0cdf449723f7093ad5535dabf1a716e25c1a
boomermath <63861512+boomermath@users.noreply.github.com>
2020-08-13 16:56:40 -0400

    Update hey.js

--- commands/hey.js
+++ commands/hey.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: 'hey',
     description: "this is a ping command!",
-    if (message.content === `-hey`) {
-	message.channel.send(`hey`);
+    if (message.content === '-hey') {
+	message.channel.send('hey');
 }
 }
 
