31691730910a0f56ed440da5f534664765c82778
kinglalu <56965132+kinglalu@users.noreply.github.com>
2020-08-13 13:25:45 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '-';
  
+client.commands = new Discord.Collection();
+ 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+ 
+    client.commands.set(command.name, command);
+}
 
 client.on('ready', () => {
 
